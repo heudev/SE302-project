28149ed85e64c5ea73eab8624d92ffd107d38d13
heudev <hulkienesuysal@gmail.com>
2024-10-17 14:45:45 +0300

    initial commit with boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-sky-500 font-bold text-2xl">
+      <div>SE302 Project</div>
+    </div>
+  )
+}
+
+export default App
