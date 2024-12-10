6ba9a7a0400d7078883b6d3d3b95d2734cf97cbf
Mert Koğuş <mertkogus@gmail.com>
2024-12-10 17:28:32 +0300

    Classroom Component implemented
    Classroom component implemented and mounted to Home page of the program.

--- src/pages/home/classrooms/index.tsx
+++ src/pages/home/classrooms/index.tsx
@@ -1,7 +1,12 @@
 import { useClassrooms } from "../../../hooks/useClassrooms";
+import { Zoom } from "react-awesome-reveal";
+import { Box, Divider, List, ListItem, ListItemButton, ListItemText, TextField, Chip } from "@mui/material";
+import { useState } from "react";
+import MeetingRoomIcon from '@mui/icons-material/MeetingRoom';
 
 export default function Classrooms() {
     const { classrooms, error } = useClassrooms();
+    const [searchTerm, setSearchTerm] = useState('');
 
     if (error) {
         console.log(error);
@@ -10,14 +15,48 @@ export default function Classrooms() {
 
     console.log(classrooms);
 
+    // Filter
+    const filteredClassrooms = classrooms.filter(classroom =>
+        classroom.Classroom.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
     return (
-        <div>
-            {classrooms.map((classroom) => (
-                <div>
-                    <h3>{classroom.Classroom}</h3>
-                    <p>Capacity: {classroom.Capacity}</p>
-                </div>
-            ))}
-        </div>
+        <Zoom style={{ width: '100%', maxWidth: 380 }} delay={700} triggerOnce={true}>
+            <Box sx={{ bgcolor: 'background.paper' }} className="shadow-2xl rounded">
+                <TextField
+                    type="search"
+                    label={searchTerm === '' ? 'Search by Classroom Name' : 'Search results'}
+                    variant="filled"
+                    autoComplete="off"
+                    fullWidth
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <Divider />
+                <List sx={{ overflowY: 'auto', height: 500 }}>
+                    {filteredClassrooms.map((classroom) => (
+                        <ListItem
+                            key={classroom.Classroom}
+                            disablePadding
+                            className="shadow-md"
+                        >
+                            <ListItemButton sx={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+                                
+                                <ListItemText
+                                    primary={classroom.Classroom}
+                                />
+                                
+                                <Chip
+                                    icon={<MeetingRoomIcon />}
+                                    label={`Capacity: ${classroom.Capacity}`}
+                                    color="primary"
+                                    variant="outlined"
+                                    className="w-36"
+                                />
+                            </ListItemButton>
+                        </ListItem>
+                    ))}
+                </List>
+            </Box>
+        </Zoom>
     );
 }
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,10 @@
 import Courses from "./courses"
-
+import Classrooms from "./classrooms"
 export default function Home() {
     return (
-        <div className="flex-grow flex justify-center">
+        <div className="flex-grow flex justify-center space-x-4">
             <Courses />
+            <Classrooms />
         </div>
     )
 }
